5ab46a9bba8f507a623ee0610431784f91c1a069
Cameron Stroebel <cameron.stroebel08@gmail.com>
2021-06-08 01:51:47 +0200

    feat: text editing and card moving

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,12 +1,70 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useContext, useEffect, useRef } from 'react';
+import { AppContext } from '../../context';
 import { TCard } from '../../types';
 import './Card.css';
 
-function Card({ id, label, description }: TCard): ReactElement {
+type CardProps = TCard & { columnId: string };
+const _labelInitialHeight = '1.75rem';
+const _descriptionInitialHeight = '1.25rem';
+
+function Card({ columnId, id, label, description }: CardProps): ReactElement {
+  const { setCardLabel, setCardDescription, moveCardToColumn } =
+    useContext(AppContext);
+  const labelRef = useRef<HTMLTextAreaElement>(null);
+  const descriptionRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleMoveCard = (direction: 'left' | 'right') => {
+    moveCardToColumn(columnId, direction, id);
+  };
+
+  useEffect(() => {
+    if (!labelRef) return;
+
+    const cardLabel = labelRef.current as HTMLTextAreaElement;
+
+    cardLabel.style.height = _labelInitialHeight;
+    cardLabel.style.height = `${cardLabel.scrollHeight}px`;
+  }, [label]);
+
+  useEffect(() => {
+    if (!descriptionRef) return;
+
+    const cardDescription = descriptionRef.current as HTMLTextAreaElement;
+
+    cardDescription.style.height = _descriptionInitialHeight;
+    cardDescription.style.height = `${cardDescription.scrollHeight}px`;
+  }, [description]);
+
   return (
     <li className="card">
-      <h3 className="card__label">{label}</h3>
-      <p className="card__description">{description}</p>
+      <button
+        className="card__move-button card__move-button--left"
+        title="Click to move this card to the previous column"
+        onClick={() => handleMoveCard('left')}
+      >
+        {'<'}
+      </button>
+      <button
+        className="card__move-button card__move-button--right"
+        title="Click to move this card to the next column"
+        onClick={() => handleMoveCard('right')}
+      >
+        {'>'}
+      </button>
+      <textarea
+        className="card__label"
+        onChange={({ target: { value } }) => setCardLabel(columnId, id, value)}
+        ref={labelRef}
+        value={label}
+      />
+      <textarea
+        className="card__description"
+        onChange={({ target: { value } }) =>
+          setCardDescription(columnId, id, value)
+        }
+        ref={descriptionRef}
+        value={description}
+      />
     </li>
   );
 }
--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,20 +1,43 @@
-import React, { ReactElement, useContext } from 'react';
+import React, {
+  memo,
+  ReactElement,
+  useContext,
+  useEffect,
+  useRef,
+} from 'react';
 
 import { Card } from '../Card';
 import { TColumn } from '../../types';
 import { AppContext } from '../../context';
 import './Column.css';
 
+const _labelInitialHeight = '1.875rem';
+
 function Column({ id, label, cards }: TColumn): ReactElement {
-  const { addCard } = useContext(AppContext);
+  const { addCard, setColumnLabel } = useContext(AppContext);
+  const labelRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (!labelRef) return;
+
+    const columnLabel = labelRef.current as HTMLTextAreaElement;
+
+    columnLabel.style.height = _labelInitialHeight;
+    columnLabel.style.height = `${columnLabel.scrollHeight}px`;
+  }, [label]);
 
   return (
     <li className="column" data-testid="column">
-      <h2 className="column__label">{label}</h2>
+      <textarea
+        className="column__label"
+        onChange={({ target: { value } }) => setColumnLabel(id, value)}
+        ref={labelRef}
+        value={label}
+      />
       <ul className="column__cards-list" data-testid="card-list">
         <>
           {cards.map((card) => (
-            <Card key={card.id} {...card} />
+            <Card key={card.id} columnId={id} {...card} />
           ))}
           <li className="column__add-card">
             <button
@@ -32,4 +55,4 @@ function Column({ id, label, cards }: TColumn): ReactElement {
   );
 }
 
-export default Column;
+export default memo(Column);
