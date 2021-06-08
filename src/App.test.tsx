604bcfc1178ef34dedc0f2904e9ba00f2fbed87b
Cameron Stroebel <cameron.stroebel08@gmail.com>
2021-06-08 11:07:12 +0200

    chore: update tests
    Should've been done along with the features

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -13,7 +13,7 @@ test('renders the default state', () => {
   expect(heading).toBeInTheDocument();
   expect(columns.length).toBe(2);
   expect(cardLists[0].children.length).toBe(2);
-  expect(cardLists[1].children.length).toBe(3);
+  expect(cardLists[1].children.length).toBe(4);
 });
 
 test('adds a new column', () => {
@@ -33,4 +33,38 @@ test('adds a new card', () => {
 
   userEvent.click(screen.getAllByText('+ Add New Card')[0]);
   expect(firstCardList.children.length).toBe(3);
+});
+
+test('edits a column label', () => {
+  render(<App />);
+
+  const typedText = "Todo (haven't started)";
+  const label = screen.getByText('Column 1');
+
+  userEvent.clear(label);
+  userEvent.type(label, typedText);
+  expect(label.textContent).toBe(typedText);
+});
+
+test('edits a card label', () => {
+  render(<App />);
+
+  const typedText = 'First task';
+  const label = screen.getByText('> Click me! <');
+
+  userEvent.clear(label);
+  userEvent.type(label, typedText);
+  expect(label.textContent).toBe(typedText);
+});
+
+test('edits a card description', () => {
+  render(<App />);
+  userEvent.click(screen.getAllByText('+ Add New Card')[0]);
+
+  const typedText = 'This is a new task';
+  const description = screen.getByText('Click here to edit description');
+
+  userEvent.clear(description);
+  userEvent.type(description, typedText);
+  expect(description.textContent).toBe(typedText);
 });
